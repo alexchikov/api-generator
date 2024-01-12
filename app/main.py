d9f438c3466174d40fdf0315df9a255c09249207
alexchikov <alex.chikov@outlook.com>
2024-01-12 14:15:53 +0300

    Done with orders generator

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+import http
+
 
 app = FastAPI()
 
 @app.get('/')
 def get_root():
     return {"message":"Hello"}
+
+@app.get("/orders/{id}")
+def get_order(id):
+    dir_ls = os.listdir('../files/orders')
+    for filename in dir_ls:
+        print(dir_ls)
+        if filename.split('_')[1] == str(id):
+            return FileResponse(f'../files/orders/{filename}', 
+                                filename=filename,
+                                media_type="applicatino/octet-stream")
+    return http.HTTPStatus.NOT_FOUND
--- generator/order.py
+++ generator/order.py
@@ -1,17 +1,28 @@
+from time import sleep
+from datetime import datetime
 import json
+import os
 import random
+import logging
+from sys import stdout
+
+
+logging.basicConfig(level=logging.INFO, stream=stdout,
+                    format="%(levelname)s %(asctime)s: %(message)s ")
+
 
 def generate_order(id):
     customer_gender = random.choice(('male', 'female'))
-    with open('./files/samples/names.json') as file:
+    with open('../files/samples/names.json') as file:
         names = json.load(file)
         customer_name = f'{random.choice(names["names"][customer_gender])} {random.choice(names["surnames"][customer_gender])}'
-    with open("./files/samples/addresses.json") as file:
+    with open("../files/samples/addresses.json") as file:
         addresses = json.load(file)
         address_dict = random.choice(addresses)
         address = f'{address_dict["street"]}, {address_dict["house"]}, {address_dict["city"]}'
-    restaurant_id = random.randint(1,20)
-    contact_number = "+7"+"".join([str(random.randint(0,9)) for _ in range(9)])
+    restaurant_id = random.randint(1, 20)
+    contact_number = "+7" + \
+        "".join([str(random.randint(0, 9)) for _ in range(9)])
     items = generate_order_items()
     status = "Preparing"
     return {
@@ -22,11 +33,12 @@ def generate_order(id):
         "restaurantid": restaurant_id,
         "items": items,
         "status": status
-        }
-    
+    }
+
+
 def generate_order_items():
     items_list = list()
-    with open("./files/samples/items.json") as file:
+    with open("../files/samples/items.json") as file:
         items = json.load(file)
     for _ in range(random.randint(1, 6)):
         while True:
@@ -41,5 +53,15 @@ def generate_order_items():
                            "price": item["price"]*quantity})
     return items_list
 
+
 if __name__ == "__main__":
-    print(generate_order(1))
+    latest_id = 1
+    while True:
+        creation_time = datetime.strftime(datetime.now(), '%d-%m-%Y_%H:%M:%S')
+        filename = f"order_{latest_id}_{creation_time}.json"
+        with open(f"../files/orders/{filename}", "w") as file:
+            order = generate_order(latest_id)
+            json.dump(order, file, ensure_ascii=False, indent=4)
+        latest_id += 1
+        logging.info(f'successfully generated new file {filename}')
+        sleep(random.randint(10, 30))
